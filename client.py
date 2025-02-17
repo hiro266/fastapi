58f3698845237acb7fc1c1605011487444ab205d
hiro266 <t1freejob@gmail.com>
2025-02-18 00:29:54 +0900

    update

--- client.py
+++ client.py
@@ -1,6 +1,10 @@
 import requests
+import asyncio
+import time
 
-res = requests.get('http://localhost:8000')
+res = requests.get('http://localhost:8000/sample/', headers={"Authorization": "Bearer XXX"})
+# res = requests.post('http://localhost:8000/items/', json={"name": "サッカーボール", "price": 1000, "description": "サッカーボールです"})
 
 print(res.status_code)
 print(res.text)
+print(res.headers)
--- main.py
+++ main.py
@@ -1,8 +1,50 @@
-from fastapi import FastAPI
+import asyncio
+from typing import Union
+from fastapi import FastAPI, Header, Response
+from pydantic import BaseModel
 
 app = FastAPI()
 
-# ↑appオブジェクトに対して、デコレーターを使ってルーティングを設定
+# -------------------------------------------------------------
+# GET
+
+items = ["item1", "item2", "item3", "item4", "item5"]
+# appオブジェクトに対して、デコレーターを使ってルーティングを設定
 @app.get("/")
 def read_root():
     return {"message": "Hello World"}
+
+@app.get("/items/")
+# skipとlimitはクエリパラメータ → skip=どこまでスキップするか, limit=いくつ取得するか
+def read_items(skip: int = 0, limit: int = 10):
+    return {"items": items[skip : skip + limit]}
+
+@app.get("/items/{item_id}")
+def read_item(item_id):
+    return {"item_id": item_id, "item_name": "サッカーボール"}
+
+# -------------------------------------------------------------
+# Post
+
+# BaseModelを継承すると、インスタンス変数を定義できて型チェックができる
+class Item(BaseModel):
+    name: str
+    price: float
+    description: Union[str, None] = None
+
+@app.post("/items/")
+def create_item(item: Item):
+    # 下記出力はAPI側で確認するためのもの
+    print(f"実務ではデータベースに保存処理を書く: {item.name}, {item.price}, {item.description}")
+    return item
+
+# -------------------------------------------------------------
+# Header
+
+@app.get("/sample/")
+def read_sample(
+    response: Response,
+    authorization: Union[str, None] = Header(default=None)):
+    print(authorization)
+    response.headers["custom-header"] = "12345"
+    return {"message": "ヘッダー情報を取得しました"}
