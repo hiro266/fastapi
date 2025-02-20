82b14c0676e5d9095d3eca0bc2e906e6d7ebe46e
hiro266 <t1freejob@gmail.com>
2025-02-20 22:42:31 +0900

    cors設定

--- main.py
+++ main.py
@@ -2,8 +2,17 @@ import asyncio
 from typing import Union
 from fastapi import FastAPI, Header, Response
 from pydantic import BaseModel
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],  # または ["*"] で全て許可
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # -------------------------------------------------------------
 # GET
